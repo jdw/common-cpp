680fc758f055049d2cf101825ce88167f09ff5bd
Johannes Wirde <jwirde@kth.se>
2009-03-17 11:16:05 +0100

    Update

--- jdw_3dmath.h
+++ jdw_3dmath.h
@@ -1,44 +1,88 @@
 #ifndef JDW_V2
 #define JDW_V2
 template <class T>
-class v2 {
+class V2 {
 public:	
-	v2(T in_x, T in_y) {
+	V2(T in_x, T in_y) {
 		this->x = in_x;
 		this->y = in_y;
 	}
 	
-	v2() {
+	V2() {
 		this->x = (T)0;
 		this->y = (T)0;
 	}
 	
-	T DP(const v2& in) { // Dot-product
+	T GetDP(const V2& in) const { // Dot-product
 		return this->x * in.x + this->y * in.y;
 	}
 	
-	v2 XP(const v2& in) { // Cross-prodcut
-		return v2(this->x * in.x, this->y * in.y);
+	V2 GetXP(const V2& in) const { // Cross-prodcut
+		return V2(this->x * in.x, this->y * in.y);
 	}
 	
-	T GetLength() { // Gets length of vector
-		return sqrt(this->DP(*this));
+	T GetLength() const { // Gets length of vector
+		return sqrt(this->GetDP(*this));
 	}
 	
-	T GetDist(const v2& in) { // Returns distance between two vectors
+	T GetDist(const V2& in) { // Returns distance between two vectors
 		T dX = this->x - in.x;
 		T dY = this->y - in.y;
 		
 		return sqrt(dX * dX + dY * dY);
 	}
 	
-	v2 GetUnit() {
+	V2 GetUnit() {
 		T length = this->GetLength();
-		
-		return v2(this->x / length, this->y / length);
+		return V2(this->x / length, this->y / length);
 	}
 	
+	void operator=(const V2<T>& in) {
+		this->x = in.x;
+		this->y = in.y;
+	}
+
+	bool operator==(const V2<T>& in) const {
+		if (this->x != in.x) return false;
+		if (this->y != in.y) return false;
+		
+		return true;
+	}
+
+	bool operator!=(const V2<T>& in) const {
+		if (this->x == in.x) return false;
+		if (this->y == in.y) return false;
+		
+		return true;
+	}
+
+	void operator+=(const V2<T>& in) {
+		this->x += in.x;
+		this->y += in.y;
+	}
 	
+	V2<T> operator+(const V2<T>& in) const {
+		return V2<T>(this->x + in.x, this->y + in.y);
+	}
+	
+	void operator-=(const V2<T>& in) {
+		this->x -= in.x;
+		this->y -= in.y;
+	}
+
+	V2<T> operator-(const V2<T>& in) const {
+		return V2<T>(this->x - in.x, this->y - in.y);
+	}
+
+	void operator*=(const T& in) {
+		this->x *= in;
+		this->y *= in;
+	}
+
+	V2<T> operator*(const T& in) const {
+		return V2<T>(this->x * in, this->y * in);
+	}
+
 	T x, y;
 };
 #endif
@@ -46,50 +90,99 @@ public:
 #ifndef JDW_V3
 #define JDW_V3
 template <class T>
-class v3 {
+class V3 {
 public:	
-	v3(T in_x, T in_y, T in_z) {
+	V3(T in_x, T in_y, T in_z) {
 		this->x = in_x;
 		this->y = in_y;
 		this->z = in_z;
 	}
 	
-	v3() {
+	V3() {
 		this->x = (T)0;
 		this->y = (T)0;
 		this->z = (T)0;
 	}
 	
-	T DP(const v3<T> in) { // Dot-product
+	T GetDP(const V3<T> in) const { // Dot-product
 		return this->x * in.x + this->y * in.y + this->z * in.z;
 	}
 	
-	v3 XP(const v3<T>& in) { // Cross-prodcut
+	V3 GetXP(const V3<T>& in) const { // Cross-prodcut
 		// A x B -> A=this, B=in
-		return v3<T>((this->y * in.z) - (this->z * in.y),
+		return V3<T>((this->y * in.z) - (this->z * in.y),
 		             (this->z * in.x) - (this->x * in.z),
 		             (this->x * in.y) - (this->y * in.x));
 	}
 	
-	T GetLength() { // Gets length of vector
-		return sqrt(this->DP(*this));
+	T GetLength() const { // Gets length of vector
+		return sqrt(this->GetDP(*this));
 	}
 	
-	T GetDist(const v3& in) { // Returns distance between two vectors
+	T GetDist(const V3& in) const { // Returns distance between two vectors
 		T dX = this->x - in.x;
 		T dY = this->y - in.y;
 		T dZ = this->z - in.z;
-		
 		return sqrt(dX * dX + dY * dY + dZ * dZ);
 	}
 	
-	v3 GetUnit() { // Returns a unit vector
+	V3 GetUnit() { // Returns a unit vector
 		T length = this->GetLength();
-		
-		return v3(this->x / length, this->y / length, this->z / length);
+		return V3(this->x / length, this->y / length, this->z / length);
 	}
 	
+	void operator=(const V2<T>& in) {
+		this->x = in.x;
+		this->y = in.y;
+		this->z = in.z;
+	}
+
+	bool operator==(const V2<T>& in) const {
+		if (this->x != in.x) return false;
+		if (this->y != in.y) return false;
+		if (this->z != in.z) return false;
+
+		return true;
+	}
+
+	bool operator!=(const V2<T>& in) const {
+		if (this->x == in.x) return false;
+		if (this->y == in.y) return false;
+		if (this->z == in.z) return false;
+
+		return true;
+	}
+
+	void operator+=(const V2<T>& in) {
+		this->x += in.x;
+		this->y += in.y;
+		this->z += in.z;
+	}
 	
+	V2<T> operator+(const V2<T>& in) const {
+		return V2<T>(this->x + in.x, this->y + in.y, this->z + in.z);
+	}
+	
+	void operator-=(const V2<T>& in) {
+		this->x -= in.x;
+		this->y -= in.y;
+		this->z -= in.z;
+	}
+
+	V2<T> operator-(const V2<T>& in) const {
+		return V2<T>(this->x - in.x, this->y - in.y, this->z - in.z);
+	}
+
+	void operator*=(const T& in) {
+		this->x *= in;
+		this->y *= in;
+		this->z *= in;
+	}
+
+	V2<T> operator*(const T& in) const {
+		return V2<T>(this->x * in, this->y * in, this->z * in);
+	}
+
 	T x, y, z;
 };
 #endif
@@ -97,8 +190,8 @@ public:
 #ifndef JDW_VERTEX
 #define JDW_VERTEX
 template <class T>
-struct vertex {
-	vertex() {
+struct Vertex {
+	Vertex() {
 		this->pos.x = (T)0;
 		this->pos.y = (T)0;
 		this->pos.z = (T)0;
@@ -106,25 +199,25 @@ struct vertex {
 		this->scr.y = (T)0;
 		
 		calculated = false;
-		pNext = (vertex<float>*)0;
+		pNext = (Vertex<float>*)0;
 	}
 	
-	v3<T> pos;
-	v2<int> scr;
+	V3<T> pos;
+	V2<int> scr;
 	bool calculated;
-	vertex* pNext;
+	Vertex* pNext;
 };
 #endif
 
 #ifndef JDW_POLY
 #define JDW_POLY
 
-#include "PixelToaster.h"
+#include <PixelToaster-1.4/PixelToaster.h>
 
 template <class T>
-class poly {
+class Poly {
 public:	
-	poly(vertex<T>* in_o, vertex<T>* in_a, vertex<T>* in_b) {
+	Poly(Vertex<T>* in_o, Vertex<T>* in_a, Vertex<T>* in_b) {
 		this->o = in_o; // Keep 'em counterclockwise!
 		this->a = in_a;
 		this->b = in_b;
@@ -133,20 +226,20 @@ public:
 		
 	}
 	
-	v3<T> GetNormal() {
+	V3<T> GetNormal() {
 		//normal = (p1-p2) x (p3-p2) dvs,
 		//normal = (a - o) x (b - o) 
-		return (v3<T>(a->pos.x - o->pos.x, a->pos.y - o->pos.y, a->pos.z - o->pos.z).XP(
-		        v3<T>(b->pos.x - o->pos.x, b->pos.y - o->pos.y, b->pos.z - o->pos.z))).GetUnit();
+		return (V3<T>(a->pos.x - o->pos.x, a->pos.y - o->pos.y, a->pos.z - o->pos.z).XP(
+		        V3<T>(b->pos.x - o->pos.x, b->pos.y - o->pos.y, b->pos.z - o->pos.z))).GetUnit();
 	}
 	
-	T GetK(const v3<T>& p) {
+	T GetK(const V3<T>& p) {
 		return this->GetNormal().DP(p);
 	}
 	
-	vertex<T>* o;
-	vertex<T>* a;
-	vertex<T>* b;
+	Vertex<T>* o;
+	Vertex<T>* a;
+	Vertex<T>* b;
 	PixelToaster::TrueColorPixel clr;
 };
 #endif
