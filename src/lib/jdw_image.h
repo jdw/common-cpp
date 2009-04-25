3ba5626546101fbde1f50e8a415eb1ad37ba3874
jdw <jdw@80x25.se>
2009-04-25 11:16:00 +0200

    Adding DrawMode for pixels with alpha and additive blending, amongst others. Boosting stuff with shared pointers.

--- src/lib/jdw_image.h
+++ src/lib/jdw_image.h
@@ -8,37 +8,41 @@
 #ifndef JDW_IMAGE
 #define JDW_IMAGE
 
-//#include <vector>
+#include <vector>
 #include "jdw_test.h"
+#include <stdlib.h>
+#include "jdw_types.h"
+#include "jdw_pixel.h"
+#include <boost/shared_ptr.hpp>
 
 namespace JDW_PixelDrawMode {
 	enum Enum {
 		COPY = 0, // pixel[x][y] = in_pixel;
 		ADD, // pixel[x][y] += in_pixel;
+		ALPHA, // pixel[x][y].r = (255-in_pixel.a) * pixel[x][y].r + in_pixel.a * in_pixel.r;
 		MULTI, // pixel[x][y] *= in_pixel;
-		ONBLACKONLY, // if (pixel[x][y].r == 0 && pixel[x][y].g == 0 && pixel[x][y].b == 0) pixel[x][y] = in_pixel;
-		ONREDONLY, // if (pixel[x][y].g == 0 && pixel[x][y].b == 0) pixel[x][y] = in_pixel;
-		ONGREENONLY, // if (pixel[x][y].r == 0 && pixel[x][y].b == 0) pixel[x][y] = in_pixel;
-		ONBLUEONLY, // if (pixel[x][y].r == 0 && pixel[x][y].g == 0) pixel[x][y] = in_pixel;
-		ONGREYONLY, // if (pixel[x][y].r != 0 && pixel[x][y].g != 0 && pixel[x][y].b != 0) pixel[x][y] = in_pixel;
-		ONWHITEONLY, // if (pixel[x][y].r == 255 && pixel[x][y].g == 255 && pixel[x][y].b == 255) pixel[x][y] = in_pixel;
+		ONBLACKONLY, // if (pixel[x][y]->r == 0 && pixel[x][y]->g == 0 && pixel[x][y]->b == 0) pixel[x][y] = in_pixel;
+		ONREDONLY, // if (pixel[x][y]->g == 0 && pixel[x][y]->b == 0) pixel[x][y] = in_pixel;
+		ONGREENONLY, // if (pixel[x][y]->r == 0 && pixel[x][y]->b == 0) pixel[x][y] = in_pixel;
+		ONBLUEONLY, // if (pixel[x][y]->r == 0 && pixel[x][y]->g == 0) pixel[x][y] = in_pixel;
+		ONGREYONLY, // if (pixel[x][y]->r != 0 && pixel[x][y]->g != 0 && pixel[x][y]->b != 0) pixel[x][y] = in_pixel;
+		ONWHITEONLY, // if (pixel[x][y]->r == 255 && pixel[x][y]->g == 255 && pixel[x][y]->b == 255) pixel[x][y] = in_pixel;
 
 		MAX // Faulty
 	};
 }
 
-template <class T>
+template <class T, class U> // T = shared_ptr<U>
 class JDW_Image {
 public:
-	JDW_Image(const iV2& in_size)
-	: size(in_size) {
-		//pixel = (T*)malloc(size.x * size.y * sizeof(T));
-		pixel = new T[size.x * size.y];
-		//pixel = std::vector<T>();
-		for (int i = 0; i < size.x * size.y; ++i)
-			pixel[i] = T();
+	JDW_Image(const iV2& in_size) {
+		spSize = boost::shared_ptr<iV2>(new iV2(in_size));
+		spPixel = boost::shared_ptr<std::vector<U> >(new std::vector<U>(spSize->x * spSize->y));
 
-		trans = T();
+		for (int i = 0; i < spSize->x * spSize->y; ++i)
+			(*spPixel)[i] =U();
+
+		trans = U();
 	}
 /*
 	JDW_Image(const iV2 in_size, T* in_pixel)
@@ -50,7 +54,7 @@ public:
 		//delete[] pixel;
 	}
 
-	void Blit(const iV2& in_offset, JDW_Image* const in_pFrom, const JDW_PixelDrawMode::Enum in_mode = JDW_PixelDrawMode::COPY) {
+	void Blit(const iV2& in_offset, JDW_Image<T, U>* const in_pFrom, const JDW_PixelDrawMode::Enum in_mode = JDW_PixelDrawMode::COPY) {
 		for (int y = 0; y < in_pFrom->GetSize().y; ++y)
 			for (int x = 0; x < in_pFrom->GetSize().x; ++x)
 				this->PutPixel(iV2(x + in_offset.x, y + in_offset.y),
@@ -58,43 +62,63 @@ public:
 					           in_mode);
 	}
 
-	void PutPixel(const iV2& in_pos, const T& in_pixel, const JDW_PixelDrawMode::Enum in_mode = JDW_PixelDrawMode::COPY) {
+	void PutPixel(const iV2& in_pos, const U& in_pixel, const JDW_PixelDrawMode::Enum in_mode = JDW_PixelDrawMode::COPY) {
 		TEST_TRUE(in_mode < JDW_PixelDrawMode::MAX);
 		if (!(IsInside(in_pos))) return;
-		int tmp_pos = in_pos.x + size.x * in_pos.y;
+		int tmp_pos = in_pos.x + spSize->x * in_pos.y;
 		switch (in_mode) {
-			case JDW_PixelDrawMode::COPY: pixel[tmp_pos] = in_pixel; break;
-			case JDW_PixelDrawMode::ADD: pixel[tmp_pos].r += in_pixel.r; pixel[tmp_pos].g += in_pixel.g; pixel[tmp_pos].b += in_pixel.b; break;
-			case JDW_PixelDrawMode::MULTI: pixel[tmp_pos].r *= in_pixel.r; pixel[tmp_pos].g *= in_pixel.g; pixel[tmp_pos].b *= in_pixel.b; break;
-			case JDW_PixelDrawMode::ONBLACKONLY: if (pixel[tmp_pos].r == 0 && pixel[tmp_pos].g == 0 && pixel[tmp_pos].b == 0) pixel[tmp_pos] = in_pixel; break;
-			case JDW_PixelDrawMode::ONREDONLY: if (pixel[tmp_pos].g == 0 && pixel[tmp_pos].b == 0) pixel[tmp_pos] = in_pixel; break;
-			case JDW_PixelDrawMode::ONGREENONLY: if (pixel[tmp_pos].r == 0 && pixel[tmp_pos].b == 0) pixel[tmp_pos] = in_pixel; break;
-			case JDW_PixelDrawMode::ONBLUEONLY: if (pixel[tmp_pos].r == 0 && pixel[tmp_pos].g == 0) pixel[tmp_pos] = in_pixel; break;
-			case JDW_PixelDrawMode::ONGREYONLY: if (pixel[tmp_pos].r != 0 && pixel[tmp_pos].g != 0 && pixel[tmp_pos].b != 0) pixel[tmp_pos] = in_pixel; break;
-			case JDW_PixelDrawMode::ONWHITEONLY: if (pixel[tmp_pos].r == 255 && pixel[tmp_pos].g == 255 && pixel[tmp_pos].b == 255) pixel[tmp_pos] = in_pixel; break;
+			case JDW_PixelDrawMode::COPY: (*spPixel)[tmp_pos] = in_pixel; break;
+			case JDW_PixelDrawMode::ADD: (*spPixel)[tmp_pos].r += in_pixel.r; (*spPixel)[tmp_pos].g += in_pixel.g; (*spPixel)[tmp_pos].b += in_pixel.b; break;
+			case JDW_PixelDrawMode::ALPHA: {
+				double tmp_a = double(in_pixel.a);
+				double tmp_ad = tmp_a / 255.0;
+
+				double tmp_r, tmp_g, tmp_b;
+				tmp_r = (1.0 - tmp_ad) * double((*spPixel)[tmp_pos].r) + tmp_ad * double(in_pixel.r);
+				tmp_r = (tmp_r > 255)? 255: tmp_r;
+				tmp_g = (1.0 - tmp_ad) * double((*spPixel)[tmp_pos].g) + tmp_ad * double(in_pixel.g);
+				tmp_g = (tmp_r > 255)? 255: tmp_g;
+				tmp_b = (1.0 - tmp_ad) * double((*spPixel)[tmp_pos].b) + tmp_ad * double(in_pixel.b);
+				tmp_b = (tmp_r > 255)? 255: tmp_b;
+
+				(*spPixel)[tmp_pos].r = int(tmp_r);
+				(*spPixel)[tmp_pos].g = int(tmp_g);
+				(*spPixel)[tmp_pos].b = int(tmp_b);
+			} break;
+
+			case JDW_PixelDrawMode::MULTI: (*spPixel)[tmp_pos].r *= in_pixel.r; (*spPixel)[tmp_pos].g *= in_pixel.g; (*spPixel)[tmp_pos].b *= in_pixel.b; break;
+			case JDW_PixelDrawMode::ONBLACKONLY: if ((*spPixel)[tmp_pos].r == 0 && (*spPixel)[tmp_pos].g == 0 && (*spPixel)[tmp_pos].b == 0) (*spPixel)[tmp_pos] = in_pixel; break;
+			case JDW_PixelDrawMode::ONREDONLY: if ((*spPixel)[tmp_pos].g == 0 && (*spPixel)[tmp_pos].b == 0) (*spPixel)[tmp_pos] = in_pixel; break;
+			case JDW_PixelDrawMode::ONGREENONLY: if ((*spPixel)[tmp_pos].r == 0 && (*spPixel)[tmp_pos].b == 0) (*spPixel)[tmp_pos] = in_pixel; break;
+			case JDW_PixelDrawMode::ONBLUEONLY: if ((*spPixel)[tmp_pos].r == 0 && (*spPixel)[tmp_pos].g == 0) (*spPixel)[tmp_pos] = in_pixel; break;
+			case JDW_PixelDrawMode::ONGREYONLY: if ((*spPixel)[tmp_pos].r != 0 && (*spPixel)[tmp_pos].g != 0 && (*spPixel)[tmp_pos].b != 0) (*spPixel)[tmp_pos] = in_pixel; break;
+			case JDW_PixelDrawMode::ONWHITEONLY: if ((*spPixel)[tmp_pos].r == 255 && (*spPixel)[tmp_pos].g == 255 && (*spPixel)[tmp_pos].b == 255) (*spPixel)[tmp_pos] = in_pixel; break;
 			default: TEST_TRUE(FALSE); break; // If new modes are implemented, we will be told...
 		}
 	}
 
-	T GetTrans() { return trans; }
-	T* GetPixels() { return pixel; }
+	U GetTrans() { return trans; }
+	void SetTrans(const U& in) { trans = in; }
+	std::vector<U>* GetPixels() { return spPixel.get(); }
 	//std::vector<T> GetPixels() { return pixel; }
-	void SetPixels(T* in_p) { pixel = in_p; }
-	void SetTrans(const T& in) { trans = in; }
-	const T GetPixel(const iV2 in_pos) { return pixel[in_pos.x + size.x * in_pos.y]; }
-	const iV2& GetSize() { return size; }
+	//void SetPixels(T in_p) { pixel = in_p; }
+	const U& GetPixel(const iV2& in_pos) { return (*spPixel)[in_pos.x + spSize->x * in_pos.y]; }
+	const iV2& GetSize() { return (*spSize.get()); }
+	void SetAlpha(const int& in_alpha) {
+		if (in_alpha > 255) return;
 
-	void Black() {
-		for (int i = 0; i < size.x * size.y; ++i) {
-			pixel[i].r = 0;
-			pixel[i].g = 0;
-			pixel[i].b = 0;
-		}
+		for (int i = 0; i < spSize->x * spSize->y; ++i)
+			(*spPixel)[i].a = in_alpha;
+	}
+
+	void Fill(const T& in_color) {
+		for (int i = 0; i < spSize->x * spSize->y; ++i)
+			(*spPixel)[i].integer = in_color.integer;
 	}
 
 	bool IsInside(const iV2& in_pos) {
-		if (!(0 <= in_pos.x && in_pos.x < size.x)) return FALSE;
-		if (!(0 <= in_pos.y && in_pos.y < size.y)) return FALSE;
+		if (!(0 <= in_pos.x && in_pos.x < spSize->x)) return FALSE;
+		if (!(0 <= in_pos.y && in_pos.y < spSize->y)) return FALSE;
 
 		return TRUE;
 	}
@@ -162,12 +186,13 @@ public:
 		}
 	}
 
-	const iV2 size;
-	T* pixel;
-	//std::vector<T> pixel;
-	T trans;
+	boost::shared_ptr<iV2> spSize;
+	boost::shared_ptr<std::vector<U> > spPixel;
+	U trans;
 };
 
-typedef JDW_Image<PixelToaster::TrueColorPixel> Image;
+typedef JDW_Image<JDW_Pixel, JDW_Pixel> Image;
+typedef boost::shared_ptr<JDW_Image<SPPixel, JDW_Pixel> > SPImage;
+//typedef JDW_Image<PixelToaster::TrueColorPixel> Image;
 
 #endif
