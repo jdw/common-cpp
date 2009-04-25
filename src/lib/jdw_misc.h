341209810b595a2b38a1e115a9871bba08ad43cd
jdw <jdw@80x25.se>
2009-04-25 11:18:54 +0200

    Commenting out abs due to collision with stdlib, adding guards.

--- src/lib/jdw_misc.h
+++ src/lib/jdw_misc.h
@@ -1,16 +1,21 @@
 #ifndef JDW_MISC
 #define JDW_MISC
 
+/*
 #ifndef abs(x)
 #define abs(x) (((x) > 0)? (x): -1 * (x))
 #endif
+*/
 
 #ifndef PI
 #define PI 3.14159265;
 #endif
 //#define E = 0.01f
+
+#ifndef FPS
 // ~60 FPS
 #define FPS 0.0166666667
+#endif
 
 #ifndef FALSE
 #define FALSE 0
